0c892908ad02523df364f405b674fa243d7b9d2f
Frank Hui <huifrank@live.com>
2020-12-29 12:32:52 -0800

    Add some input stuff

---
+++ convert.py
@@ -0,0 +1,7 @@
+image_path = input("Path for image: ")
+display_size = input("Display size? (l/s): ")
+while display_size is not "l" or display_size is not "s":
+    print("Invalid input for display size. must be l or s.")
+    display_size = input("Display size? (l/s): ")
+output_path = input("Output save location?: ")
+
